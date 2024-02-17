af36165020f6d85888a37d74428cc36add354d1e
RONLUG <ronnakorn.lugsamipichate@gmail.com>
2024-02-17 13:29:23 +0700

    Add margin when flex wrap

---
+++ src/lib/sections/Footer.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Section from "$lib/components/Section.svelte";
+    import "iconify-icon";
+</script>
+
+<Section clazz="pb-10 gap-20">
+    <footer class="flex flex-row gap-20">
+        <div>
+            <h2 class="text-2xl font-semibold">Get In Touch</h2>
+            <ul>
+                <li>
+                    <a class="hover:underline" href="mailto: ron.lugsami@gmail.com">ron.lugsami@gmail.com</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h2 class="text-2xl font-semibold">More About Me</h2>
+            <ul>
+                <li>
+                    <a class="hover:underline" href="https://github.com/RONLUG" target="_blank">github.com</a>
+                </li>
+                <li>
+                    <a class="hover:underline" href="https://ronlug.itch.io/" target="_blank">itch.io</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</Section>
--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -2,14 +2,14 @@
     import Section from "$lib/components/Section.svelte";
 </script>
 
-<Section clazz="w-screen h-screen justify-center items-center">
+<Section clazz="h-screen justify-center items-center">
     <div class="flex w-full flex-col items-center justify-center gap-x-16 gap-y-5 xl:w-2/3 xl:flex-row">
         <div class="image-container">
             <img src="/img/portrait.jpg" class="bg-cover" alt="portrait" />
         </div>
-        <div class="mx-12 flex flex-col">
-            <h1 class="text-5xl font-extrabold">Hello, <br /> Call Me Ronnakorn</h1>
-            <p>University student graduated in 2025, Front-end developer</p>
+        <div class="flex flex-col">
+            <h1 class="px-12 text-5xl font-extrabold xl:px-0">Hello, <br /> Call Me Ronnakorn</h1>
+            <p class="px-12 xl:px-0">University student graduated in 2025, Front-end developer</p>
         </div>
     </div>
 </Section>
